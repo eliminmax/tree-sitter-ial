7af337b184b9e6fbe9acb55566c620912c920aa7
Eli Array Minkoff <eli@planetminkoff.com>
2026-01-23 16:07:21 -0500

    Attempt to fix language loading (again)

--- lua/tree-sitter-ial/init.lua
+++ lua/tree-sitter-ial/init.lua
@@ -1,16 +1,19 @@
 local M = {}
 
 function M.setup(arg)
-    require("nvim-treesitter.parsers").ial = {
-        install_info = {
-            vim.fn.expand("%:p:h:h:h"),
-            branch = "main",
-            generate = true,
-            generate_from_json = true,
-            queries = 'queries',
-        }
+    vim.api.nvim_create_autocmd('User', { pattern = 'TSUpdate',
+        callback = function()
+            require("nvim-treesitter.parsers").ial = {
+                install_info = {
+                    vim.fn.expand("%:p:h:h:h"),
+                    branch = "main",
+                    generate = true,
+                    generate_from_json = true,
+                    queries = 'queries',
+                }
+            }
+        end
     }
-
 end
 
 return M
