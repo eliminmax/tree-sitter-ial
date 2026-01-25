0c3579515ec055d1fce529524f71321b64429f36
Eli Array Minkoff <eli@planetminkoff.com>
2026-01-25 17:14:35 -0500

    Rework highlight groups

--- queries/ial/highlights.scm
+++ queries/ial/highlights.scm
@@ -1,7 +1,7 @@
 (comment) @comment
 (ascii_string) @string
 (ascii_char_literal) @number
-(ascii_char_escaped) @string.special
+(ascii_char_escaped) @string.escape
 "DATA" @keyword
 "ASCII" @keyword
 
@@ -9,13 +9,13 @@
 (sub) @operator
 (mul) @operator
 (div) @operator
-(label (identifier) @variable) @punctuation.delimiter
+(label(_) ":" @punctuation.special)
 (sep) @punctuation.delimiter
 (expression) @expression
 (unary_op (sub) (expression (number))) @number
 (instruction _ @function.builtin)
 (number) @number
-(identifier) @variable
+(identifier) @label
 (lparen) @punctuation.bracket
 (rparen) @punctuation.bracket
 (parameter (immediate "#" @attribute (expression)))
