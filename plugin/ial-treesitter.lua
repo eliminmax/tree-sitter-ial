6c74360d39cba5b3b9c5ea1a5b81564fa744054f
Eli Array Minkoff <eli@planetminkoff.com>
2026-01-25 17:38:34 -0500

    Neovim plugin attempt

---
+++ plugin/ial-treesitter.lua
@@ -0,0 +1,13 @@
+-- hook into TSUpdate
+vim.api.nvim_create_autocmd('User', { pattern = 'TSUpdate',
+    callback = function()
+        require("nvim-treesitter.parsers").ial = {
+            install_info = {
+                path = vim.fn.expand('<sfile>:p:h:h'),
+                generate = true,
+                generate_from_json = false,
+                queries = 'queries/ial',
+            }
+        }
+    end
+})
